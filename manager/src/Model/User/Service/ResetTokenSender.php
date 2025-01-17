86d87e711b54918b708b6453ec3ee7bda0d9df9e
Dmitriy Chengaev <dmchengaev@yandex.ru>
2025-01-17 16:21:15 +0500

    mail config

--- manager/src/Model/User/Service/ResetTokenSender.php
+++ manager/src/Model/User/Service/ResetTokenSender.php
@@ -10,25 +10,26 @@ use Twig\Environment;
 
 class ResetTokenSender
 {
-    private $mailer;
-    private $twig;
+  private $mailer;
+  private $twig;
 
-    public function __construct(\Swift_Mailer $mailer, Environment $twig)
-    {
-        $this->mailer = $mailer;
-        $this->twig = $twig;
+  public function __construct(\Swift_Mailer $mailer, Environment $twig)
+  {
+    $this->mailer = $mailer;
+    $this->twig = $twig;
+  }
+
+  public function send(Email $email, ResetToken $token): void
+  {
+    $message = (new \Swift_Message('Password resetting'))
+      ->setFrom($_ENV['MAILER_FROM_EMAIL'])
+      ->setTo($email->getValue())
+      ->setBody($this->twig->render('mail/user/reset.html.twig', [
+        'token' => $token->getToken()
+      ]), 'text/html');
+
+    if (!$this->mailer->send($message)) {
+      throw new \RuntimeException('Unable to send message.');
     }
-
-    public function send(Email $email, ResetToken $token): void
-    {
-        $message = (new \Swift_Message('Password resetting'))
-            ->setTo($email->getValue())
-            ->setBody($this->twig->render('mail/user/reset.html.twig', [
-                'token' => $token->getToken()
-            ]), 'text/html');
-
-        if (!$this->mailer->send($message)) {
-            throw new \RuntimeException('Unable to send message.');
-        }
-    }
+  }
 }
